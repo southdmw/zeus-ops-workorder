2c5cb142e480b707044dbd70315237dc4c720c93
southdmw <southdmw@gmail.com>
2025-11-17 15:41:05 +0800

    Update AssistantController.java

--- src/main/java/com/gdu/zeus/ops/workorder/client/AssistantController.java
+++ src/main/java/com/gdu/zeus/ops/workorder/client/AssistantController.java
@@ -2,24 +2,35 @@ package com.gdu.zeus.ops.workorder.client;
 
 import com.gdu.zeus.ops.workorder.services.CustomerSupportAssistant;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+
 
 @RequestMapping("/api/assistant")
 @RestController
 public class AssistantController {
 
-	private final CustomerSupportAssistant agent;
+    private final CustomerSupportAssistant agent;
 
-	public AssistantController(CustomerSupportAssistant agent)  {
-		this.agent = agent;
-	}
+    public AssistantController(CustomerSupportAssistant agent) {
+        this.agent = agent;
+    }
 
-    @RequestMapping(path="/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-	public Flux<String> chat(@RequestParam(name = "chatId") String chatId,
-							 @RequestParam(name = "userMessage") String userMessage) {
-		return agent.chat(chatId, userMessage);
-	}
+    @CrossOrigin
+    @RequestMapping(path = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> chat(@RequestParam(name = "chatId") String chatId,
+                             @RequestParam(name = "userMessage") String userMessage) {
+        return agent.chat(chatId, userMessage);
+    }
+
+    @CrossOrigin
+    @RequestMapping(path = "/chatByUserId", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> chat(
+            @RequestParam(name = "userId") String userId,
+            @RequestParam(name = "chatId") String chatId,
+            @RequestParam(name = "userMessage") String userMessage) {
+        return agent.chat(userId, chatId, userMessage);
+    }
+
 }
