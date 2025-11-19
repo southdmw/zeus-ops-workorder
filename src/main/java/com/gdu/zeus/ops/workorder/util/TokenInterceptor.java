04f394ccb31b88ce3bca4d29dbd5aee58201edba
southdmw <southdmw@gmail.com>
2025-11-19 10:31:33 +0800

    Update TokenInterceptor.java

--- src/main/java/com/gdu/zeus/ops/workorder/util/TokenInterceptor.java
+++ src/main/java/com/gdu/zeus/ops/workorder/util/TokenInterceptor.java
@@ -1,1 +1,25 @@
+package com.gdu.zeus.ops.workorder.util;
 
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+/**
+ * RestTemplate拦截器,自动添加token到请求header
+ */
+public class TokenInterceptor implements ClientHttpRequestInterceptor {
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body,
+                                        ClientHttpRequestExecution execution) throws IOException {
+        // 从ThreadLocal获取token
+        String token = TokenContext.getToken();
+        // 如果token存在,添加到请求header
+        if (token != null && !token.isEmpty()) {
+            request.getHeaders().set("Authorization", "Bearer " + token);
+        }
+        return execution.execute(request, body);
+    }
+}
